292d749fca444e390406f7145f2505f39e4a9958
Dawood Ahmad <extdah@brreg.no>
2025-04-03 12:31:15 +0200

    Escape Room init

---
+++ index.js
@@ -0,0 +1,10 @@
+const CANVAS_WIDTH = 512
+const CANVAS_HEIGHT = 512
+
+const canvas = document.getElementById("canvas");
+canvas.style.minWidth = `${CANVAS_WIDTH}px`;
+canvas.style.maxWidth = `${CANVAS_WIDTH}px`;
+canvas.style.minHeight = `${CANVAS_HEIGHT}px`;
+canvas.style.maxHeight = `${CANVAS_HEIGHT}px`;
+
+const TILE_SIZE = 32;
